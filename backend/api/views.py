3467a277cfc8428d29517b55e2685905630ea434
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-21 13:39:29 +0530

    Extracting Request's Body

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 def api_home(request, **args):
-    return JsonResponse({'message': 'Django API Response!'})
+    body = request.body
+    data = {}
+
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+    print(data)
+
+    data['headers'] = dict(request.headers)
+    print(request.headers)
+
+    data['content_type'] = request.content_type
+
+    return JsonResponse({'message': 'Django API Response'})
 
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,11 @@
 import requests
 
-endpoint ="https://httpbin.org/status.200"
-endpoint ="https://httpbin.org/"
+# endpoint ="https://httpbin.org/status.200"
+# endpoint ="https://httpbin.org/"
 endpoint = "http://localhost:8000/api/"
 
 # API (Application Programming Interface) -> Method
-get_response = requests.get(endpoint) 
+get_response = requests.get(endpoint, params={'abc': 123}, json={'query': "Hello World"}) 
+
 print(get_response.text)
 print(get_response.status_code)
