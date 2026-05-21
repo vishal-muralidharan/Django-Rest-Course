2fdacfdbcc1c6d3a7078b6cbc2ae76b1f8ca68b6
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-21 17:13:34 +0530

    Serialiser on POST

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,6 @@
 from django.forms.models import model_to_dict 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import JsonResponse
  
 from products.models import Product
 from products.serialisers import ProductSerializer
@@ -9,7 +8,12 @@ from products.serialisers import ProductSerializer
 @api_view(["POST"])
 def api_home(request, **args):
     data = request.data
-    return JsonResponse(data)
+
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
         # json_data_str = json.dumps(data )
 
     # return HttpResponse(data, headers={'content-type': 'application/json'})
