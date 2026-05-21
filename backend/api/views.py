829f43b81248196731302c4554dbb610ba126b5e
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-21 16:16:39 +0530

    api_view and Rest Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict 
-import json
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+ 
 from products.models import Product
 
+@api_view(["GET"])
 def api_home(request, **args):
     model_data = Product.objects.all().order_by("?").first()
 
@@ -12,8 +12,8 @@ def api_home(request, **args):
 
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-        
-        json_data_str = json.dumps(data )
+    return JsonResponse(data)
+        # json_data_str = json.dumps(data )
 
-    return HttpResponse(data, headers={'content-type': 'application/json'})
+    # return HttpResponse(data, headers={'content-type': 'application/json'})
 
