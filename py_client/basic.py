3fb8f6649bbd68fe4b25f0420e254bd7b3ee765f
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-21 13:11:41 +0530

    Basic Python API Setup

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,9 @@ import requests
 
 endpoint ="https://httpbin.org/status.200"
 endpoint ="https://httpbin.org/"
+endpoint = "http://localhost:8000/"
 
 # API (Application Programming Interface) -> Method
 get_response = requests.get(endpoint) 
 print(get_response.text)
+print(get_response.status_code)
