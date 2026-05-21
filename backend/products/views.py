868d3b0d6f76f628428d953a1abdb0816e9918f7
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-21 17:49:51 +0530

    CreateAPIViews Implementation

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,26 @@ from rest_framework import generics
 from products.models import Product
 from products.serialisers import ProductSerializer
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        print(serializer.validated_data)
+
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+
+        if content is None:
+            content = title
+
+        serializer.save(content=content)
+
+product_create_view = ProductCreateAPIView.as_view()
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
