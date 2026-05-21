c353ea2e395a4fd00dd958ff8197acd6dd238f3b
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-21 16:49:03 +0530

    Serialiser - I

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,16 +3,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
  
 from products.models import Product
+from products.serialisers import ProductSerializer
 
 @api_view(["GET"])
 def api_home(request, **args):
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
 
     data = {}
 
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
+    if instance:
+        # data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
         # json_data_str = json.dumps(data )
 
     # return HttpResponse(data, headers={'content-type': 'application/json'})
