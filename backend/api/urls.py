7c958341e44bd0fd2c57f81ee35d39b4647cbbcc
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-23 10:21:19 +0530

    JWT Authentication - Setup

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 from . import views
 
 urlpatterns = [
+    # DRF token auth
     path('auth/', obtain_auth_token),
+    # Simple JWT endpoints
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # API root
     path('', views.api_home)
 ]
--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -132,6 +132,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "api.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
