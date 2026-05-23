c8172151dbf829ba96e1b8873aba5cdd852adeb4
Vishal Muralidharan <vishmuralee1006@gmail.com>
2026-05-23 10:55:57 +0530

    Django CORS Headers

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'algoliasearch_django',
@@ -49,12 +50,21 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS settings - allow the local JS client to call the API
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8811',
+    'https://localhost:8811',
+]
+# Only apply CORS to API endpoints
+CORS_URLS_REGEX = r"^/api/.*$"
 
 ROOT_URLCONF = 'cfehome.urls'
 
